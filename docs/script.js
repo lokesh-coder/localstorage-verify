103d3854f83515080c30934ad1bfafd2ba031042
Lokesh Rajendran <lokesh_r@external.mckinsey.com>
2024-04-05 15:48:25 +0530

    add indexdb

--- docs/script.js
+++ docs/script.js
@@ -18,3 +18,34 @@ button.addEventListener("click", () => {
 setInterval(() => {
   window.location.reload();
 }, 5000);
+
+(async () => {
+  // document.body.classList.add("idb-fail");
+  // if (!localStorage.getItem("loggedin")) return;
+
+  const db = await idb.openDB("auth", 1, {
+    upgrade(db) {
+      if (!db.objectStoreNames.contains("user")) {
+        console.log("created new user");
+        db.createObjectStore("user", { keyPath: "id" });
+      }
+    },
+  });
+  const hasUser = await db.get("user", 1);
+  const tx = await db.transaction("user", "readwrite");
+
+  if (!hasUser && localStorage.getItem("loggedin")) {
+    document.body.classList.add("idb-success");
+    console.log("new user");
+    await tx.store.add({
+      id: 1,
+      created_at: new Date(),
+    });
+    await tx.done;
+  } else if (!localStorage.getItem("loggedin") && !hasUser) {
+    document.body.classList.add("idb-fail");
+  } else {
+    document.body.classList.add("idb-success");
+    console.log("user exists");
+  }
+})();
